8e9ed812f3c1062e93a2681e8c0de2f315b72eb3
Sören <soerendip@users.noreply.github.com>
2021-07-13 09:22:29 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,17 @@
 from setuptools import setup, find_packages
 
+install_requires = [
+    'bintrees',
+    'matplotlib',
+    'numpy',
+    'pandas',
+    'pymzML',
+    'scipy'
+    'torch>=1.2.0',
+    'tqdm'
+]
+
+
 config = {
     'description': 'Installable python library for peak detection in mzML files.',
     'author': 'Soren Wacker',
